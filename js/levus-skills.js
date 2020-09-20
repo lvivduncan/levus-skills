4967ec28bdf1bc4046d134dc4f4a9cb88c42eb0e
lvivduncan <lvivduncan@gmail.com>
2020-09-20 15:25:28 +0300

    scroll

--- js/levus-skills.js
+++ js/levus-skills.js
@@ -1,9 +1,17 @@
 {
-    // 19-09-2020
+    // 20-09-2020
     const skills = document.querySelectorAll('.levus-skills');
+    // height window
+    const start = document.documentElement.clientHeight;
 
-    skills.forEach(skill => delay(skill, skill.dataset.result, skill.dataset.speed));
-
+    window.addEventListener('scroll', () => {
+        const change = skills[0].getBoundingClientRect().top;
+        const sum = start - change + 10;
+        if (sum === 0 || sum === 1 || sum === 2 || sum === 3 || sum === 4 || sum === 5 || sum === 6 || sum === 7) {
+            skills.forEach(skill => delay(skill, skill.dataset.result, skill.dataset.speed));
+        }
+    });
+    
     function delay(selector, num = 2, speed = 1){
         for (let index = 0; index <= num; index++) {
             setTimeout( () => {
